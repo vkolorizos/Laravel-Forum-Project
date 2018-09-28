56fbf29b102cea1762d2480d2a95fd4df0fe84e4
Vasilhs <vasilhs_xd@hotmail.com>
2018-09-28 14:29:07 +0300

    Added reply form + minor css update

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/threads','ThreadController@index');
+Route::get('/threads','ThreadController@index')->name('threads.index');
 Route::get('/threads/{thread}','ThreadController@show');
 Route::post('/threads/{thread}/replies','ReplyController@store');
