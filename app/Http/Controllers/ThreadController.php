344e75fae583923bf4545072b73dcb9ec19eadc7
Vasilhs <vasilhs_xd@hotmail.com>
2018-09-30 01:34:28 +0300

    Adding Filters + Refactoring

--- app/Http/Controllers/ThreadController.php
+++ app/Http/Controllers/ThreadController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Channel;
 use App\Thread;
+use App\Filters\ThreadFilters;
 use Illuminate\Http\Request;
 
 class ThreadController extends Controller
@@ -18,24 +19,12 @@ class ThreadController extends Controller
 	 * Display a listing of the resource.
 	 *
 	 * @param Channel $channel
+	 * @param ThreadFilters $filters
 	 * @return \Illuminate\Http\Response
 	 */
-	public function index(Channel $channel)
+	public function index(Channel $channel, ThreadFilters $filters)
 	{
-
-		if ($channel->exists) {
-			$threads = $channel->threads()->latest();
-		}else {
-			$threads = Thread::latest();
-		}
-
-		if($username = request('by')){
-			$user = \App\User::where('name', $username)->firstOrFail();
-
-			$threads->where('user_id', $user->id);
-		}
-
-		$threads = $threads->get();
+		$threads = $this->getThreads($channel, $filters);
 
 		return view('threads.index', compact('threads'));
 	}
@@ -118,4 +107,22 @@ class ThreadController extends Controller
 	{
 		//
 	}
+
+	/**
+	 * @param Channel $channel
+	 * @param ThreadFilters $filters
+	 * @return mixed
+	 */
+	protected function getThreads(Channel $channel, ThreadFilters $filters)
+	{
+		$threads = Thread::latest()->filter($filters);
+
+		if ($channel->exists) {
+			$threads->where('channel_id', $channel->id);
+		}
+
+		$threads = $threads->get();
+		return $threads;
+	}
+
 }
--- app/Thread.php
+++ app/Thread.php
@@ -33,4 +33,8 @@ class Thread extends Model
     {
     	$this->replies()->create($reply);
     }
+
+    public function scopeFilter($query, $filters){
+    	return $filters->apply($query);
+    }
 }
